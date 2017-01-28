0b6d1c5e780577ebffcef5eb2a12fcf81d39fbdd
William Betts <wbetts@equalexperts.com>
2017-01-28 16:42:34 +0000

    Implemented initial board

--- src/main/scala/chess/Board.scala
+++ src/main/scala/chess/Board.scala
@@ -13,7 +13,7 @@ sealed trait BoardState {
   def toMove: Colour
 }
 
-case object InitialBoard extends BoardState with Board {
+case class SuccessorBoard(board: Board, move: Move) extends BoardState with Board {
   override def pieceAt(square: Square): Piece = ???
 
   override def currentPositions(predicate: (Piece) => Boolean): Map[Square, Piece] = ???
@@ -21,10 +21,21 @@ case object InitialBoard extends BoardState with Board {
   override def toMove: Colour = ???
 }
 
-case class SuccessorBoard(board: Board, move: Move) extends BoardState with Board {
-  override def pieceAt(square: Square): Piece = ???
+case object InitialBoard extends BoardState with Board {
+  private val pieces = (0 to 7).map(x => Square(x, 1) -> WhitePawn).toMap ++
+    (0 to 7).map(x => Square(x, 6) -> BlackPawn).toMap +
+    (Square(0,0) -> WhiteRook) + (Square(7,0) -> WhiteRook) +
+    (Square(1,0) -> WhiteKnight) + (Square(6,0) -> WhiteKnight) +
+    (Square(2,0) -> WhiteBishop) + (Square(5,0) -> WhiteBishop) +
+    (Square(3,0) -> WhiteQueen) + (Square(4,0) -> WhiteKing) +
+    (Square(0,7) -> BlackRook) + (Square(7,7) -> BlackRook) +
+    (Square(1,7) -> BlackKnight) + (Square(6,7) -> BlackKnight) +
+    (Square(2,7) -> BlackBishop) + (Square(5,7) -> BlackBishop) +
+    (Square(3,7) -> BlackQueen) + (Square(4,7) -> BlackKing)
 
-  override def currentPositions(predicate: (Piece) => Boolean): Map[Square, Piece] = ???
+  override def pieceAt(square: Square): Piece = pieces(square)
 
-  override def toMove: Colour = ???
+  override def currentPositions(predicate: (Piece) => Boolean) = pieces.filter(t => predicate(t._2))
+
+  override def toMove = White
 }
